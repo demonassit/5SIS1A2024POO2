9051f8db1d9f56760d7142979a3e0c55e7e22f2c
demonassit <jminorg@ipn.mx>
2024-01-16 11:04:25 -0600

    primer programa

---
+++ 01PrimeraChamba/HolaMundo.java
@@ -0,0 +1,23 @@
+
+
+// vamos a crear un primer programar
+
+/* para poder comentar en diferentes lineas de codigo */
+
+public class HolaMundo{
+
+    //los metodos son los comportamientos que va a tener una clase u objeto. El comportamiento son las acciones u operaciones para las que esta destinada la clase
+
+    //existen metodos principales (main) 
+    //metodos getter and setter intercambio de datos 
+    //metodos constructores los constructores son los encargados de la estructura de la clase, inicializar las variables, crear las instancias 
+    //metodos por tipo de dato nos referimos a int, float, String, etc
+
+    //los objetos son instancia (invocaciones) la construccion abstracta de la cosa
+
+    //las caracteristicas del objeto son los atributos de la clase
+
+    public static void main(String[] args) {
+        System.out.println("Si sale error date baja de la vida");
+    }
+}
